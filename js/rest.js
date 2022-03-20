d0d4218c4551aad2c4b390d3c96c1075a3a1dd77
Md Akbar Hossain <akbarhossain160@gmail.com>
2022-03-20 12:53:03 +0600

    first commit

--- js/rest.js
+++ js/rest.js
@@ -10,20 +10,37 @@ const displayCountry = data => {
     // }
     const container = document.getElementById( 'country' );
     data.forEach( country => {
-        console.log( country );
+        // console.log( country );
         const div = document.createElement( 'div' );
         div.classList.add( 'style' );
         div.innerHTML = `
         <h1>Name: ${ country.name }</h1>
-        <h2>Native Name: ${ country.nativeName }</h2>
-        <h3>Capital : ${ country.capital }</h3>
-        <h3>Language : ${ country.languages[ 0 ].name }</h3>
-        <h3>Population : ${ country.population }</h3>
-        <h3>Area : ${ country.area }, SFT</h3>
-        <h4>Sub Region : ${ country.subregion }</h4>
-        <img src="${ country.flag }">
+        <img src="${ country.flag }"><br/>
+        <button onclick="loadCountryByName('${ country.name }')">Details</button>
         `;
         container.appendChild( div );
 
     } );
+};
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${ name }`;
+    fetch( url )
+        .then( res => res.json() )
+        .then( data => displayCountryDetails( data[ 0 ] ) );
+};
+
+const displayCountryDetails = country => {
+
+    console.log( country );
+    const countryDetails = document.getElementById( 'country-details' );
+    // const div = document.createElement( 'div' );
+    countryDetails.innerHTML = `
+        <h1>Name: ${ country.name.common }</h1>
+        <img src="${ country.flags.png }"><br/>
+        <h3>Capital : ${ country.capital }</h3>
+        <h3>Population : ${ country.population }</h3>
+        <h3>Area : ${ country.area }, SFT</h3>
+        <h4>Sub Region : ${ country.subregion }</h4>
+    `;
 };
